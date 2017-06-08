9f4e280be2f0d7945e13b298db9e1523961d9db1
Eremin <mitya1705@gmail.com>
2017-06-08 09:43:16 +0000

    proto:

---
+++ url-shortener.js
@@ -0,0 +1,26 @@
+const config = require('config');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+
+var list = {};
+var currentId = 0;
+
+app.get('/new/:url', function(req, res){  
+    list[(++currentId).toString()] = req.params.url;
+    res.send(list)
+}) 
+
+app.get('/:id', function(req, res){
+    var id = req.params.id;
+    if(id in list){
+        res.send(list[id])    
+    }
+    else{
+        res.status(404).send('no such element');
+    }
+})
+
+app.listen(port, function () {
+  console.log('Example app listening on port '+port)
+})
